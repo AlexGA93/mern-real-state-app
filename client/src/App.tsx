4f8ac9a48ae48df5a4bc472ff26edf50ff58e44f
Alejandro Gimeno <alexgimat@hotmail.com>
2024-01-19 16:36:19 +0100

    first commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, SignIn, SignUp, About, Profile } from './pages';
+
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='/sign-in' element={ <SignIn /> } />
+        <Route path='/sign-up' element={ <SignUp /> } />
+        <Route path='/about' element={ <About /> } />
+        <Route path='/profile' element={ <Profile /> } />
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ client/src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as About } from './About';
+export { default as Home } from './Home';
+export { default as Profile } from './Profile';
+export { default as SignIn } from './SignIn';
+export { default as SignUp } from './SignUp';
