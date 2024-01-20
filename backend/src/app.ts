4936d4dea1c8afc9e98d577f45d5a1e1c9a77bd9
Alejandro Gimeno <alexgimat@hotmail.com>
2024-01-20 10:22:58 +0100

    error handler middleware

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express, Request, Response, json } from 'express';
+import express, { Express, NextFunction, Request, Response, json } from 'express';
 import cors from 'cors';
 import { databaseConnection } from './database/database.config';
 import testRouter from './routes/test.route';
@@ -11,6 +11,17 @@ const app: Express = express();
 // config
 app.use(cors());
 app.use(express.json());
+
+// middleware - error handling
+app.use((err: any, req: Request, res: Response, next: NextFunction) => {
+  const statusCode: number = err.statusCode || 500;
+  const message: string = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  })
+});
 
 // routes
 app.use('/api/test', testRouter);
