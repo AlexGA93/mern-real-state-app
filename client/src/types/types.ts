5e29b741082acaa9178bb625254aaf992fe8b811
Alejandro Gimeno <alexgimat@hotmail.com>
2024-01-20 17:11:14 +0100

    react-redux andredux-toolkit added with store and dispatch actions

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,5 +1,11 @@
-export interface SignUpType {
+export interface UserType {
   username?: string,
   email: string,
   password: string
+}
+
+export interface UserStoreType {
+  currentUser: null | UserType,
+  error: null | Error,
+  loading: boolean
 }
